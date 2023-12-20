897622fd24c3809aa7915d6713b560ce37dd9a70
abdirahman-abass-diss <abdirahman.diss@student.moringaschool.com>
2023-12-20 07:33:11 +0300

    finished

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]
+    
+    while len(fib_sequence) < length:
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+    
+    print(fib_sequence[:length])
+
+# Test the function
+print_fibonacci(9)
